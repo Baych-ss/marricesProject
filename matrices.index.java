09a64c3b41bc4bba902b13b18843e6d3369e66a8
Baych <baielkalmakov07@gmail.com>
2024-09-20 21:07:56 +0600

    ships ready

---
+++ matrices.index.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String user_name = scan.nextLine();
+		int[] array = new Array[7];
+		int bigShip1, bigShip2, bigShip3, bigShip4, normShip1, normShip11;
+		int normShip2, normShip21, miniShip1, miniShip2, miniShip3, miniShip4;
+		bigShip1 = 0;
+		bigShip2 = 0;
+		bigShip3 = 0;
+		bigShip4 = 0;
+		normShip1 = 0;
+		normShip11 = 0;
+		normShip2 = 0;
+		normShip21 = 0;
+		miniShip1 = 0;
+		miniShip2 = 0;
+		miniShip3 = 0;
+		miniShip4 = 0;
+		for(int i = 0; i < 7; i++){
+		    for(int j = 0; j < 7; j++){
+		        
+		    }
+		}
+	}
+}
